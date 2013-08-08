e1f58b53ae53d9748a6abf0b483ab088551bf975
Dave Syer <dsyer@gopivotal.com>
2013-08-08 11:37:13 +0100

    Add new relic monitoring

--- src/main/resources/main.sh
+++ src/main/resources/main.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/sh -x
 
 # Use spring.profiles.active to switch between main site and indexer app, e.g.
 # --spring.profiles.active=indexer for indexer.  Site runs in default and site profiles.
 
-java -Xmx1260m -XX:MaxPermSize=512m -classpath "." org.springframework.boot.loader.JarLauncher $*
+PATH=.java/bin:$PATH
+
+if [ -f lib/newrelic*.jar ]; then
+  mv lib/newrelic-agent*.jar newrelic.jar
+  JAVA_AGENT=-javaagent:newrelic.jar
+fi
+
+java -XX:OnOutOfMemoryError=./.buildpack-diagnostics/killjava -Xss1M -Xmx1260m -XX:MaxPermSize=1024m ${LOCAL_OPTS} ${JAVA_AGENT} -cp . org.springframework.boot.loader.JarLauncher $*
